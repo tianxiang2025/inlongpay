31ec8663936260765a4fb5b9d5023c4f21faa1f8
tianxiang2025 <s9community69@gmail.com>
2024-11-06 04:39:28 +0800

    Update paybot.php

--- paybot.php
+++ paybot.php
@@ -1,9 +1,9 @@
-# inlongpay
 <?php
 $botToken = "6326129798:AAEGu6XnqX0tXjUzG62eNwgIvLiL8Eo80Pc";
 $website = "https://api.telegram.org/bot".$botToken;
 
 $update = file_get_contents("php://input");
+file_put_contents('log.txt', $update . PHP_EOL, FILE_APPEND);
 $updateArray = json_decode($update, TRUE);
 
 $chatId = $updateArray["message"]["chat"]["id"];
